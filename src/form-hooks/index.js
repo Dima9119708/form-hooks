e2653bc6be98dc3536435055f08b42c4867def46
dima <dmitrykh@wizardsdev.com>
2022-07-29 19:54:18 +0300

    progress

--- src/form-hooks/index.js
+++ src/form-hooks/index.js
@@ -1,15 +1,15 @@
-import useForm from "./hooks/useForm";
-import useWatch from "./hooks/useWatch/useWatch";
-import useController from "./hooks/useController";
-import useFormMethods from "./hooks/useFormMethods";
-import FormProvider from "./FormProvider";
-import useFomContext from "./hooks/useFomContext";
+import useForm from "./hooks/useForm"
+import useWatch from "./hooks/useWatch/useWatch"
+import useController from "./hooks/useController"
+import useFormMethods from "./hooks/useFormMethods"
+import FormProvider from "./FormProvider"
+import useFomContext from "./hooks/useFomContext"
 
 export {
-    useForm,
-    useWatch,
-    useController,
-    useFormMethods,
-    FormProvider,
-    useFomContext,
+  useForm,
+  useWatch,
+  useController,
+  useFormMethods,
+  FormProvider,
+  useFomContext,
 }
